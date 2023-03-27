dd6cca6e97f64798bbeab422d35e12cb645a10b5
sara <pehlivansara@hotmail.com>
2023-03-27 16:24:52 +0200

    addDBserie

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+
 use App\Form\SerieType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SerieController extends AbstractController
 {
@@ -15,12 +19,33 @@ class SerieController extends AbstractController
         return $this->render('serie/serie.html.twig');
     }
 
-    #[Route('/ajouter/serie', name: 'ajouter_serie')]
-    public function ajouterSerie(): Response
-    {
-        $formulaire = $this->createForm (SerieType::class);
-        $vars = ['formulaire'=> $formulaire->createView()];
 
-        return $this->render('serie/ajouter_serie.html.twig', $vars);
+
+    #[Route('/ajouter/serie', name: 'ajouter_serie')]
+    public function ajouterSerie(Request $req, ManagerRegistry $doctrine): Response
+    {
+        
+        $serie = new Serie();
+
+        $formulaire = $this->createForm (SerieType::class, $serie);
+
+        $formulaire->handleRequest($req);
+
+        if ($formulaire-> isSubmitted()){
+            //prendre donnée et ajouter dans la db 
+            $em = $doctrine->getManager();
+            $em->persist($serie);
+            $em->flush();
+            //et rediriger vers la page de la fiche de la série
+            return new Response("Série enregistrer dna sla db");
+           
+        }
+        else {
+            $vars = ['formulaire'=> $formulaire->createView()];
+    
+            return $this->render('serie/ajouter_serie.html.twig', $vars);
+        }
+
+
     }
 }
